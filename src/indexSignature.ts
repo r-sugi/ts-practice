b0f0697bb82a55f6c61ba7ca7a763d66dacf5836
r-sugi <r.sugimoto0615@gmail.com>
2020-05-04 21:57:43 +0900

    4-2-12 indexSignature

---
+++ src/indexSignature.ts
@@ -0,0 +1,35 @@
+// Property 'name' of type 'string' is not assignable to string index type 'number'.ts(2411)
+// type User2 = {
+//   // name: string // Error
+//   [k: string]: number
+// }
+
+type User3 = {
+  name: string // No Error
+  [k: string]: number | string
+}
+
+const userA: User3 = {
+  name: 'Taro',
+  age: 26
+}
+
+const x = userA.name
+const y = userA.age
+
+
+type Answer = 'mighty' | 'lot' | 'few' | 'entirely'
+type User4 = {
+  name: string
+  enquete: { [k: string]: Answer | undefined }
+}
+
+const userB: User4 = {
+  name: 'Taro',
+  enquete: {
+    exercise_habits: 'entirely',
+    time_of_sleeping: 'few'
+  }
+}
+const x1 = userB.enquete['exercise_habits']
+const y1 = userB.enquete['time_of_sleeping']
