17308114f8331b1297ab4035eb78eaf9b3578e9b
r-sugi <r.sugimoto0615@gmail.com>
2020-05-04 18:43:58 +0900

    1-2-6 javascriptファイルをビルドに含む

--- src/test.ts
+++ src/test.ts
@@ -1,7 +1,3 @@
-export function test1() {
-  return 'test1'
-}
-
-export function test2() {
-  return { value: 'test2'}
-}
+import { sampleText, sampleFunction } from './sample.js'
+const a = sampleFunction()
+const b = sampleText
